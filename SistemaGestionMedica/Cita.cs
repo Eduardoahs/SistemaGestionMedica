cca03b42e86621cc90109e0afd8c2c742a343027
SalvVaged <salvvaged@gmail.com>
2026-05-25 19:33:18 -0400

    Agregada la clase cita

---
+++ SistemaGestionMedica/Cita.cs
@@ -0,0 +1,34 @@
+using SistemaGestionMedica;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+public class Cita
+    {
+        public string Codigo { get; private set; }
+        public Paciente Paciente { get; set; }
+        public Medico Medico { get; set; }
+        public DateTime FechaCita { get; set; }
+        public string MotivoConsulta { get; set; }
+        public bool Atendida { get; set; }
+
+        public Cita(Paciente paciente, Medico medico, DateTime fechaCita, string motivoConsulta)
+        {
+            Codigo = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
+            Paciente = paciente;
+            Medico = medico;
+            FechaCita = fechaCita;
+            MotivoConsulta = motivoConsulta;
+            Atendida = false;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Código cita: {Codigo}");
+            Console.WriteLine($"Paciente: {Paciente.NombreCompleto}");
+            Console.WriteLine($"Médico: {Medico.NombreCompleto} ({Medico.Especialidad})");
+            Console.WriteLine($"Fecha: {FechaCita:dd/MM/yyyy HH:mm}");
+            Console.WriteLine($"Motivo: {MotivoConsulta}");
+            Console.WriteLine($"Estado: {(Atendida ? "Atendida" : "Pendiente")}");
+        }
+    }
